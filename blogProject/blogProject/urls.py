2c5dcecb21be738bea3826d93b0762472605098d
unknown <mcclaude7>
2024-12-30 13:46:00 +0200

    Added template to blog

--- blogProject/blogProject/urls.py
+++ blogProject/blogProject/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 # from django.urls import path, include
 from django.urls import path
+from blog.views import home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('blog.urls'))
+    path('', home)
 ]
